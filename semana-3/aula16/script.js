963d267cbdcbd8d9da27c00615c68d89d9f04451
Daniel Almeida <dan.felipe@live.com>
2019-09-26 13:29:55 -0300

    passo 4 e 5 done

--- semana-3/aula16/script.js
+++ semana-3/aula16/script.js
@@ -1,23 +1,33 @@
 class Despesa {
-    constructor(valor,tipo,desc){
+    constructor(valor, tipo, desc) {
         this.valor = valor;
         this.tipo = tipo;
         this.desc = desc;
+    }
+
+    getValor(){
+        return this.valor;
+    }
+    getTipo(){
+        return this.tipo;
+    }
+    getDesc(){
+        return this.desc;
     }
 }
 
 const todasAsDepesas = [];
 
-function validaDespesa(valor,tipo,desc){
-    if(parseInt(valor)<= 0 || valor===""){
+function validaDespesa(valor, tipo, desc) {
+    if (parseInt(valor) <= 0 || valor === "") {
         alert("Sem valor não existe despesa! Adicione um valor.");
         return false;
     }
-    if(tipo === ""){
+    if (tipo === "") {
         alert("Sem um tipo não poderemos filtrar! Adicione um tipo.");
         return false;
     }
-    if(desc === ""){
+    if (desc === "") {
         alert("Sem uma descrição não saberemos o que pagar! Adicione uma descrição.");
         return false;
     }
@@ -29,8 +39,33 @@ function cadastrarDespesa() {
     const tipo = document.getElementById("tipo").value;
     const desc = document.getElementById("desc").value;
 
-    if(validaDespesa(valor,tipo,desc)){
-        todasAsDepesas.push(new Despesa(valor,tipo,desc));
-        console.log(todasAsDepesas);
+    if (validaDespesa(valor, tipo, desc)) {
+        todasAsDepesas.push(new Despesa(valor, tipo, desc));
+        renderizarDespesa(todasAsDepesas,
+            document.querySelector(".despesas-list-container"));
     }
+}
+
+function renderizarDespesa(array, section) {
+    array.forEach(element => {
+        const div = document.createElement("div");
+        const valor = document.createElement("p");
+        const tipo = document.createElement("p");
+        const desc = document.createElement("p");
+
+        div.classList.add("despesas-item-container");
+
+        valor.innerHTML = element.getValor();
+        tipo.innerHTML = element.getTipo();
+        desc.innerHTML = element.getDesc();
+
+        div.append(valor,tipo,desc);
+        section.append(div);
+    });
+}
+
+function filtrarDespesa(){
+    const filtroTipo = document.getElementById("detalhe-tipo").value;
+    const filtroVmin = document.getElementById("detalhe-vmin").value;
+    const filtroVmax = document.getElementById("detalhe-vmax").value;
 }
