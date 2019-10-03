1d6a837d62131f706d19a1ab405df4265a65a540
Daniel Almeida <dan.felipe@live.com>
2019-10-03 15:01:19 -0300

    Substituido o Post.css por styled components

--- semana-4/aula20/insta4/src/components/Post/Post.js
+++ semana-4/aula20/insta4/src/components/Post/Post.js
@@ -1,25 +1,44 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './Post.css'
-import PostBottom from './../PostBottom/PostBottom.js'
+import React from "react";
+import PropTypes from "prop-types";
+import PostBottom from "./../PostBottom/PostBottom.js";
+import styled from "styled-components";
 
-class Post extends React.Component{
+const PostContainer = styled.section`
+    width: 100%;
+    border: 1px solid black;
+    margin-bottom: 30px;
+`;
 
-    render(){
+const PostHeader = styled.header`
+    display: flex;
+    padding: 5px 10px;
+`;
+const PostAvatar = styled.img`
+    max-width: 60px;
+    border-radius: 50%;
+    margin-right: 10px;
+`;
+
+const PostImage = styled.img`
+    min-width: 100%;
+    max-width: 30%;
+`;
+class Post extends React.Component {
+    render() {
         return (
-            <section className="post-container">
-                <div className="post-header">
-                    <img src={this.props.avatar} alt={this.props.nome} />
+            <PostContainer>
+                <PostHeader>
+                    <PostAvatar src={this.props.avatar} alt={this.props.nome} />
                     <h3>{this.props.nome}</h3>
-                </div>
-                <img src={this.props.postImage} alt="" className="post-image" />
-                <PostBottom/>
-            </section>
+                </PostHeader>
+                <PostImage src={this.props.postImage} alt="" />
+                <PostBottom />
+            </PostContainer>
         );
     }
 }
 
 Post.propTypes = {
     nome: PropTypes.string.isRequired
-}
-export {Post}
+};
+export { Post };
