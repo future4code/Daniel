19d206839a0b939c9dbcb1d3f5e36cb47e83aeeb
Daniel Almeida <dan.felipe@live.com>
2019-10-07 14:00:25 -0300

    Passo 3 done

--- semana-4/aula21/tarefas-react/src/components/task/Task.js
+++ semana-4/aula21/tarefas-react/src/components/task/Task.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-
+const FormTask = styled.input`
+  text-decoration: ${props => (props.isDone ? "line-through" : "none")};
+`;
 export class Task extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        name: this.props.name,
-        done: this.props.done
+      name: this.props.name,
+      done: this.props.done
     };
   }
   handleChangeInput = e => {
@@ -14,11 +16,22 @@ export class Task extends React.Component {
       name: e.target.value
     });
   };
-  render(){
-      return (
-        <div>
-            <input value={this.state.name} onChange={this.handleChangeInput}/>
-        </div>
-      );
+  handleButtonClick = () => {
+    const doneCurrentValue = this.state.done;
+    this.setState({
+      done: !doneCurrentValue
+    });
+  };
+  render() {
+    return (
+      <div>
+        <FormTask
+          value={this.state.name}
+          onChange={this.handleChangeInput}
+          isDone={this.state.done}
+        />
+        <button onClick={this.handleButtonClick}>Done!</button>
+      </div>
+    );
   }
 }
