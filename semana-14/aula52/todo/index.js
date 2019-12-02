b22d9d14fb639f7bafd8d7e67312a498e7468fc1
Daniel Almeida <dan.felipe@live.com>
2019-12-02 15:51:03 -0300

    Add color on console.log

--- semana-14/aula52/todo/index.js
+++ semana-14/aula52/todo/index.js
@@ -2,10 +2,19 @@ const args = process.argv.slice(2);
 const path = args[0];
 const task = `\n${args[1]}`;
 const fs = require('fs');
+const greenConsoleLog = "\x1b[32m";
+const resetConsoleLog = "\x1b[0m";
+const redConsoleLog = "\x1b[31m";
 
-try {
-    fs.appendFileSync(path,task,'utf8');
-    console.log("Tarefa adicionada com \x1b[32msucesso\x1b[0m!");
-} catch (error) {
-    
+if (args.length > 2) {
+    try {
+        fs.appendFileSync(path, task, 'utf8');
+        console.log(`Tarefa adicionada com ${greenConsoleLog}sucesso${resetConsoleLog}!`);
+    } catch (error) {
+        console.log(error)
+    }
+}
+else {
+    console.log(`É necessário ${redConsoleLog}2${resetConsoleLog} argumentos: `+
+    `npm start ${greenConsoleLog}arquivo.txt${resetConsoleLog} "${greenConsoleLog}tarefa${resetConsoleLog}"`)
 }
