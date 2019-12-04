91993a2bdc4295b90b60ead57a3ad3449338d4b8
Daniel Almeida <dan.felipe@live.com>
2019-12-04 16:16:42 -0300

    Add ex2.1

--- semana-14/aula54/src/ex2.ts
+++ semana-14/aula54/src/ex2.ts
@@ -1,18 +1,44 @@
 import axios, { AxiosResponse } from 'axios';
 
-const args:string[] = process.argv.slice(4);
-const action:string = args[0];
-const baseUrl = "https://jsonplaceholder.typicode.com/posts/";
+const args: string[] = process.argv.slice(4);
+const action: string = args[0];
+const posts: string[] = args.slice(1);
+const baseUrl: string = "https://jsonplaceholder.typicode.com/posts/";
+const commentsEndpoint: string = "/comments";
 
 const fetchPost = (id: string): Promise<AxiosResponse> => {
     return axios.get(`${baseUrl}${id}`);
 }
-switch(action){
+const fetchPostComments = (id: string): Promise<AxiosResponse> => {
+    return axios.get(`${baseUrl}${id}${commentsEndpoint}`);
+}
+switch (action) {
     case "getPost":
-        const post:Promise<AxiosResponse> = fetchPost(args[1]);
-        post.then((response)=>{
+        const post: Promise<AxiosResponse> = fetchPost(args[1]);
+        post.then((response) => {
             console.log(response.data);
-        },(error)=>{
+        }, (error) => {
+            console.log(error);
+        });
+        break;
+    case "getPosts":
+        const promises: Promise<AxiosResponse>[] = posts.map(id => fetchPost(id));
+        const result = Promise.all(promises);
+        result.then(
+            (values: AxiosResponse[]) => {
+                const titles: string[] = values.map(post => post.data);
+                console.log(titles)
+            }
+            , (fail: AxiosResponse[]) => {
+                console.log(fail)
+            }
+        );
+        break;
+    case "getPostComments":
+        const comments: Promise<AxiosResponse> = fetchPostComments(args[1]);
+        comments.then((response) => {
+            console.log(response.data);
+        }, (error) => {
             console.log(error);
         });
         break;
@@ -21,9 +47,4 @@ switch(action){
         break;
 
 }
-// const promises = args.map(id => fetchPost(id));
-// Promise.all(promises).then((values: AxiosResponse[]) => {
-//     const titles: string[] = values.map(post => post.data.title);
-//     console.log(titles)
-// })
 
