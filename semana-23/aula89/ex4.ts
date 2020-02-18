87b22caac572092a80ae5b240f9fb12f723a379a
Daniel Almeida <dan.felipe@live.com>
2020-02-18 15:07:10 -0300

    Add ex1~5.ts

---
+++ semana-23/aula89/ex4.ts
@@ -0,0 +1,14 @@
+function verifyIfExistRepeatedNumbers(listOfNumbers: number[]): boolean {
+    for (let i = 0; i < listOfNumbers.length; i++) {
+      if (listOfNumbers.indexOf(listOfNumbers[i]) !== i) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+/*
+
+Complexidade: O(n^2)
+
+*/
